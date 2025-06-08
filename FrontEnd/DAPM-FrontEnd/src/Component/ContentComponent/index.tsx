b0c37338ff9dfab623dfab2fe4507cae43e0f61f
ADMIN <nththuong2505@gmail.com>
2025-06-08 11:07:46 +0700

    change

--- FrontEnd/DAPM-FrontEnd/src/Component/ContentComponent/index.tsx
+++ FrontEnd/DAPM-FrontEnd/src/Component/ContentComponent/index.tsx
@@ -5,15 +5,6 @@ import NavSchbar from "./NavScheduleComponent/nav.tsx";
 import Schedule from "./ScheduleComponent/index.tsx";
 import AddSchedule from "./AddScheduleComponent/index.tsx";
 import EditSchedule from "./EditScheduleComponent/index.tsx";
-<<<<<<< HEAD
-import Profile from "./UserProfileComponent/index.tsx";
-import ChangePassword from "./ChangePassWordComponent/index.tsx";
-
-import Navbar from "./NavbarComponent/navbar.tsx"
-import UpdateAccount from "./AccountComponent/UpdateAccountComponent/account.tsx"
-import Management from './AccountComponent/ManagementComponent/management.tsx'
-import ComputerManagement from './ComputerComponent/ManagementComponent/management.tsx'
-=======
 import Navbar from "./NavbarComponent/navbar.tsx";
 import UpdateAccount from "./AccountComponent/UpdateAccountComponent/account.tsx";
 import Management from './AccountComponent/ManagementComponent/management.tsx';
@@ -31,21 +22,10 @@ const Index = () => {
     }, []);
 
     if (!user) return <div>Loading...</div>;
->>>>>>> 97d4778deb4d02a7968c18fc4f7514c63d1acf6e
 
     return (
         <div className="content">
             <div className="container-content">
-<<<<<<< HEAD
-                <NavSchbar></NavSchbar>
-                <Routes>
-                    <Route path="" element={<Schedule />} />
-                    <Route path="addschedule" element={<AddSchedule />} />
-                    <Route path="editschedule/:id" element={<EditSchedule />} />
-                    <Route path="profile" element={<Profile />} />
-                    <Route path="changepass" element={<ChangePassword />} />
-                </Routes>
-=======
                 {user.roles_id === 1 && (
                     <>
                         <Navbar />
@@ -62,7 +42,6 @@ const Index = () => {
                         </Routes>
                     </>
                 )}
->>>>>>> 97d4778deb4d02a7968c18fc4f7514c63d1acf6e
             </div>
         </div>
     );
