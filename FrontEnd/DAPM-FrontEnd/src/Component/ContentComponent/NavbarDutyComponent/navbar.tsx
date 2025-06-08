36e3cd3891d9611ece26aa572cb638ce384d545e
thang <thang@gmail.com>
2025-06-08 16:53:45 +0700

    .

---
+++ FrontEnd/DAPM-FrontEnd/src/Component/ContentComponent/NavbarDutyComponent/navbar.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import "./index.css"
+import Logo from "../../../assets/images/logo.png"
+
+const navbar = () => {
+    return (
+        <div className="content-navbar">
+            <div className="profile">
+                <img src={Logo} alt="Avatar" />
+                <div className="profile-text">
+                    <p>Xin chào,</p>
+                    <h3>Trương Như Quang Thảo</h3>
+                </div>
+            </div>
+            <div className="menu">
+                <nav>
+                    <Link to="/duty_schedule/schedule">Quản lý lịch trực</Link>
+                    <Link to="/duty_schedule/room">Quản lý sơ đồ phòng thực hành</Link>
+                    <Link to="/">Đăng xuất</Link>
+                </nav>
+            </div>
+        </div>
+    )
+}
+
+export default navbar
